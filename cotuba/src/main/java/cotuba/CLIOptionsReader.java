6dca577c4acc299389042cfb2e50b98ddd649492
Jirlan Souza <jirlansouza08@gmail.com>
2023-02-12 10:16:19 -0400

    refactor: extract CLIOptionsReader class from Main class

---
+++ cotuba/src/main/java/cotuba/CLIOptionsReader.java
@@ -0,0 +1,111 @@
+package cotuba;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
+
+public class CLIOptionsReader {
+    private Path markdownFolder;
+    private String ebookFormat;
+    private Path outputFile;
+    private boolean verboseMode = false;
+
+    public CLIOptionsReader(String[] args) throws IllegalArgumentException{
+        try {
+            var options = new Options();
+
+            var markdownFolderOption = new Option("d", "dir", true,
+                    "Diretório que contém os arquivos md. Default: diretório atual.");
+            options.addOption(markdownFolderOption);
+
+            var ebookFormatOption = new Option("f", "format", true,
+                    "Formato de saída do ebook. Pode ser: pdf ou epub. Default: pdf");
+            options.addOption(ebookFormatOption);
+
+            var outputFileOption = new Option("o", "output", true,
+                    "Arquivo de saída do ebook. Default: book.{formato}.");
+            options.addOption(outputFileOption);
+
+            var verboseModeOption = new Option("v", "verbose", false,
+                    "Habilita modo verboso.");
+            options.addOption(verboseModeOption);
+
+            CommandLineParser cmdParser = new DefaultParser();
+            var helpFormatter = new HelpFormatter();
+            CommandLine cmd;
+
+            try {
+                cmd = cmdParser.parse(options, args);
+            } catch (ParseException e) {
+                System.err.println(e.getMessage());
+                helpFormatter.printHelp("cotuba", options);
+                System.exit(1);
+                return;
+            }
+
+            String markdownFolderName = cmd.getOptionValue("dir");
+
+            if (markdownFolderName != null) {
+                markdownFolder = Paths.get(markdownFolderName);
+                if (!Files.isDirectory(markdownFolder)) {
+                    throw new IllegalArgumentException(markdownFolderName + " não é um diretório.");
+                }
+            } else {
+                markdownFolder = Paths.get("");
+            }
+
+            String ebookFormatName = cmd.getOptionValue("format");
+
+            if (ebookFormatName != null) {
+                ebookFormat = ebookFormatName.toLowerCase();
+            } else {
+                ebookFormat = "pdf";
+            }
+
+            String ebookOutputFileName = cmd.getOptionValue("output");
+            if (ebookOutputFileName != null) {
+                outputFile = Paths.get(ebookOutputFileName);
+            } else {
+                outputFile = Paths.get("book." + ebookFormat.toLowerCase());
+            }
+            if (Files.isDirectory(outputFile)) {
+                // deleta arquivos do diretório recursivamente
+                Files.walk(outputFile).sorted(Comparator.reverseOrder())
+                        .map(Path::toFile).forEach(File::delete);
+            } else {
+                Files.deleteIfExists(outputFile);
+            }
+
+            verboseMode = cmd.hasOption("verbose");
+        } catch (IOException exception) {
+            throw new IllegalArgumentException(exception);
+        }
+    }
+
+    public Path getMarkdownFolder() {
+        return markdownFolder;
+    }
+
+    public String getEbookFormat() {
+        return ebookFormat;
+    }
+
+    public Path getOutputFile() {
+        return outputFile;
+    }
+
+    public boolean isVerboseMode() {
+        return verboseMode;
+    }
+}
--- cotuba/src/main/java/cotuba/Main.java
+++ cotuba/src/main/java/cotuba/Main.java
@@ -1,30 +1,5 @@
 package cotuba;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.PathMatcher;
-import java.nio.file.Paths;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Stream;
-
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.DefaultParser;
-import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.Option;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
-import org.commonmark.node.AbstractVisitor;
-import org.commonmark.node.Heading;
-import org.commonmark.node.Node;
-import org.commonmark.node.Text;
-import org.commonmark.parser.Parser;
-import org.commonmark.renderer.html.HtmlRenderer;
-
 import com.itextpdf.html2pdf.HtmlConverter;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
@@ -33,96 +8,43 @@ import com.itextpdf.layout.element.AreaBreak;
 import com.itextpdf.layout.element.IBlockElement;
 import com.itextpdf.layout.element.IElement;
 import com.itextpdf.layout.property.AreaBreakType;
-
 import nl.siegmann.epublib.domain.Book;
 import nl.siegmann.epublib.domain.Resource;
 import nl.siegmann.epublib.epub.EpubWriter;
 import nl.siegmann.epublib.service.MediatypeService;
+import org.commonmark.node.AbstractVisitor;
+import org.commonmark.node.Heading;
+import org.commonmark.node.Node;
+import org.commonmark.node.Text;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.PathMatcher;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class Main {
 
   public static void main(String[] args) {
-    var options = new Options();
-
-    var opcaoDeDiretorioDosMD = new Option("d", "dir", true,
-        "Diretório que contém os arquivos md. Default: diretório atual.");
-    options.addOption(opcaoDeDiretorioDosMD);
-
-    var opcaoDeFormatoDoEbook = new Option("f", "format", true,
-        "Formato de saída do ebook. Pode ser: pdf ou epub. Default: pdf");
-    options.addOption(opcaoDeFormatoDoEbook);
-
-    var opcaoDeArquivoDeSaida = new Option("o", "output", true,
-        "Arquivo de saída do ebook. Default: book.{formato}.");
-    options.addOption(opcaoDeArquivoDeSaida);
-
-    var opcaoModoVerboso = new Option("v", "verbose", false,
-        "Habilita modo verboso.");
-    options.addOption(opcaoModoVerboso);
-
-    CommandLineParser cmdParser = new DefaultParser();
-    var ajuda = new HelpFormatter();
-    CommandLine cmd;
-
-    try {
-      cmd = cmdParser.parse(options, args);
-    } catch (ParseException e) {
-      System.err.println(e.getMessage());
-      ajuda.printHelp("cotuba", options);
-      System.exit(1);
-      return;
-    }
-
-    Path diretorioDosMD;
-    String formato;
-    Path arquivoDeSaida;
-    boolean modoVerboso = false;
+    var CLIOptions = new CLIOptionsReader(args);
+    var markdownFolder = CLIOptions.getMarkdownFolder();
+    var ebookFormat = CLIOptions.getEbookFormat();
+    var outputFile = CLIOptions.getOutputFile();
+    var verboseMode = CLIOptions.isVerboseMode();
 
     try {
 
-      String nomeDoDiretorioDosMD = cmd.getOptionValue("dir");
-
-      if (nomeDoDiretorioDosMD != null) {
-        diretorioDosMD = Paths.get(nomeDoDiretorioDosMD);
-        if (!Files.isDirectory(diretorioDosMD)) {
-          throw new IllegalArgumentException(nomeDoDiretorioDosMD + " não é um diretório.");
-        }
-      } else {
-        Path diretorioAtual = Paths.get("");
-        diretorioDosMD = diretorioAtual;
-      }
-
-      String nomeDoFormatoDoEbook = cmd.getOptionValue("format");
-
-      if (nomeDoFormatoDoEbook != null) {
-        formato = nomeDoFormatoDoEbook.toLowerCase();
-      } else {
-        formato = "pdf";
-      }
-
-      String nomeDoArquivoDeSaidaDoEbook = cmd.getOptionValue("output");
-      if (nomeDoArquivoDeSaidaDoEbook != null) {
-        arquivoDeSaida = Paths.get(nomeDoArquivoDeSaidaDoEbook);
-      } else {
-        arquivoDeSaida = Paths.get("book." + formato.toLowerCase());
-      }
-      if (Files.isDirectory(arquivoDeSaida)) {
-        // deleta arquivos do diretório recursivamente
-        Files.walk(arquivoDeSaida).sorted(Comparator.reverseOrder())
-            .map(Path::toFile).forEach(File::delete);
-      } else {
-        Files.deleteIfExists(arquivoDeSaida);
-      }
-
-      modoVerboso = cmd.hasOption("verbose");
-
-      if ("pdf".equals(formato)) {
-        try(var writer = new PdfWriter(Files.newOutputStream(arquivoDeSaida));
+      if ("pdf".equals(ebookFormat)) {
+        try(var writer = new PdfWriter(Files.newOutputStream(outputFile));
             var pdf = new PdfDocument(writer);
             var pdfDocument = new Document(pdf)) {
 
           PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:**/*.md");
-          try (Stream<Path> arquivosMD = Files.list(diretorioDosMD)) {
+          try (Stream<Path> arquivosMD = Files.list(markdownFolder)) {
             arquivosMD
                 .filter(matcher::matches)
                 .sorted()
@@ -167,19 +89,19 @@ public class Main {
 
                 });
           } catch (IOException ex) {
-            throw new IllegalStateException("Erro tentando encontrar arquivos .md em " + diretorioDosMD.toAbsolutePath(), ex);
+            throw new IllegalStateException("Erro tentando encontrar arquivos .md em " + markdownFolder.toAbsolutePath(), ex);
           }
 
         } catch (Exception ex) {
-          throw new IllegalStateException("Erro ao criar arquivo PDF: " + arquivoDeSaida.toAbsolutePath(), ex);
+          throw new IllegalStateException("Erro ao criar arquivo PDF: " + outputFile.toAbsolutePath(), ex);
         }
 
-      } else if ("epub".equals(formato)) {
+      } else if ("epub".equals(ebookFormat)) {
         var epub = new Book();
 
         PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:**/*.md");
-        try (Stream<Path> arquivosMD = Files.list(diretorioDosMD)) {
-          arquivosMD
+        try (Stream<Path> markdownFiles = Files.list(markdownFolder)) {
+          markdownFiles
               .filter(matcher::matches)
               .sorted()
               .forEach(arquivoMD -> {
@@ -218,25 +140,25 @@ public class Main {
                 }
               });
         } catch (IOException ex) {
-          throw new IllegalStateException("Erro tentando encontrar arquivos .md em " + diretorioDosMD.toAbsolutePath(), ex);
+          throw new IllegalStateException("Erro tentando encontrar arquivos .md em " + markdownFolder.toAbsolutePath(), ex);
         }
 
         var epubWriter = new EpubWriter();
 
         try {
-          epubWriter.write(epub, Files.newOutputStream(arquivoDeSaida));
+          epubWriter.write(epub, Files.newOutputStream(outputFile));
         } catch (IOException ex) {
-          throw new IllegalStateException("Erro ao criar arquivo EPUB: " + arquivoDeSaida.toAbsolutePath(), ex);
+          throw new IllegalStateException("Erro ao criar arquivo EPUB: " + outputFile.toAbsolutePath(), ex);
         }
       } else {
-        throw new IllegalArgumentException("Formato do ebook inválido: " + formato);
+        throw new IllegalArgumentException("Formato do ebook inválido: " + ebookFormat);
       }
 
-      System.out.println("Arquivo gerado com sucesso: " + arquivoDeSaida);
+      System.out.println("Arquivo gerado com sucesso: " + outputFile);
 
     } catch (Exception ex) {
       System.err.println(ex.getMessage());
-      if (modoVerboso) {
+      if (verboseMode) {
         ex.printStackTrace();
       }
       System.exit(1);
